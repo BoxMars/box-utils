9745e187bd8c31fa4afa4e5d706824b9148262d0
Box <me@boxz.dev>
2023-10-16 13:48:47 +0800

    UM BUS INFO

---
+++ const.js
@@ -0,0 +1,2 @@
+export const UM_BUS_URL="https://campusloop.cmdo.um.edu.mo/en_US/busstopinfo.zone:refreshzone";
+export const MACAU_BUS_URL="https://bis.dsat.gov.mo:37812/macauweb/routeLine.html?routeName=";
